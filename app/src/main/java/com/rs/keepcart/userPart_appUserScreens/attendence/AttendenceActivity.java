9001249a1c385623b712b5974481157e14bb61c2
DESKTOP-R82QVP1\Sam_Q <rajatk.ragasoft@gmail.com>
2018-07-13 14:52:53 +0530

    comit 31may

--- app/src/main/java/com/rs/keepcart/userPart_appUserScreens/attendence/AttendenceActivity.java
+++ app/src/main/java/com/rs/keepcart/userPart_appUserScreens/attendence/AttendenceActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rs.keepcart.R;
+import com.rs.keepcart.databinding.ActivityAttendenceBinding;
 
 public class AttendenceActivity extends AppCompatActivity {
 
+    private ActivityAttendenceBinding viewBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendence);
+
+
+
     }
 
 
